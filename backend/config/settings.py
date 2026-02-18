ccd3422d692de8f57f85796947d0a794d206e3ed
Piyush <piyushpatil0031@gmail.com>
2026-02-18 19:08:15 +0530

    dockerized backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,16 +76,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "support_db",
-        "USER": "postgres",
-        "PASSWORD": "Patil!0031!",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
+
 
 
 # Password validation
